2cfdefdf13665d6408af750becd7b060a3f67964
Vsevolod Romashov <7@7vn.ru>
2016-05-12 02:47:40 +0300

    adapt Runner for applying changes

--- lib/db_schema/runner.rb
+++ lib/db_schema/runner.rb
@@ -1,24 +1,38 @@
 module DbSchema
   class Runner
-    attr_reader :tables
+    attr_reader :changes
 
-    def initialize(tables)
-      @tables = tables
+    def initialize(changes)
+      @changes = changes
     end
 
     def run!
-      tables.each do |table|
-        DbSchema.connection.create_table(table.name) do
-          table.fields.each do |field|
-            field_options = {}
-            field_options[:primary_key] = true if field.primary_key?
-            field_options[:null] = false unless field.null?
-            field_options[:default] = field.default unless field.default.nil?
-
-            column(field.name, field.type, field_options)
-          end
+      changes.each do |change|
+        case change
+        when Changes::CreateTable
+          create_table(change)
+        when Changes::DropTable
+          drop_table(change)
         end
       end
+    end
+
+  private
+    def create_table(change)
+      DbSchema.connection.create_table(change.name) do
+        change.fields.each do |field|
+          field_options = {}
+          field_options[:primary_key] = true if field.primary_key?
+          field_options[:null] = false unless field.null?
+          field_options[:default] = field.default unless field.default.nil?
+
+          column(field.name, field.type, field_options)
+        end
+      end
+    end
+
+    def drop_table(change)
+      DbSchema.connection.drop_table(change.name)
     end
   end
 end
--- spec/db_schema/runner_spec.rb
+++ spec/db_schema/runner_spec.rb
@@ -1,78 +1,60 @@
 require 'spec_helper'
 
 RSpec.describe DbSchema::Runner do
-  let(:tables) do
+  before(:each) do
+    DbSchema.connection.create_table :people do
+      column :id, :integer, primary_key: true
+      column :name, :varchar
+    end
+  end
+
+  let(:users_fields) do
     [
-      DbSchema::Definitions::Table.new(
-        name: :users,
-        fields: [
-          DbSchema::Definitions::Field.new(
-            name: :id,
-            type: :integer,
-            primary_key: true
-          ),
-          DbSchema::Definitions::Field.new(
-            name: :name,
-            type: :varchar,
-            null: false
-          ),
-          DbSchema::Definitions::Field.new(
-            name: :email,
-            type: :varchar,
-            default: 'mail@example.com'
-          )
-        ]
+      DbSchema::Definitions::Field.new(
+        name: :id,
+        type: :integer,
+        primary_key: true
       ),
-      DbSchema::Definitions::Table.new(
-        name: :posts,
-        fields: [
-          DbSchema::Definitions::Field.new(
-            name: :id,
-            type: :integer,
-            primary_key: :true
-          ),
-          DbSchema::Definitions::Field.new(
-            name: :title,
-            type: :varchar
-          ),
-          DbSchema::Definitions::Field.new(
-            name: :user_id,
-            type: :integer,
-            null: false
-          )
-        ]
+      DbSchema::Definitions::Field.new(
+        name: :name,
+        type: :varchar,
+        null: false
+      ),
+      DbSchema::Definitions::Field.new(
+        name: :email,
+        type: :varchar,
+        default: 'mail@example.com'
       )
     ]
   end
 
-  subject { DbSchema::Runner.new(tables) }
+  subject { DbSchema::Runner.new(changes) }
 
-  describe '#run' do
-    it 'creates all the tables' do
-      subject.run!
+  describe '#run!' do
+    context 'with CreateTable & DropTable' do
+      let(:changes) do
+        [
+          DbSchema::Changes::CreateTable.new(name: :users, fields: users_fields),
+          DbSchema::Changes::DropTable.new(name: :people)
+        ]
+      end
 
-      expect(DbSchema.connection.tables).to eq([:users, :posts])
+      it 'applies all the changes' do
+        expect {
+          subject.run!
+        }.to change { DbSchema.connection.tables }.from([:people]).to([:users])
 
-      id, name, email = DbSchema.connection.schema(:users)
-      expect(id.first).to eq(:id)
-      expect(id.last[:db_type]).to eq('integer')
-      expect(id.last[:primary_key]).to eq(true)
-      expect(name.first).to eq(:name)
-      expect(name.last[:db_type]).to eq('character varying(255)')
-      expect(name.last[:allow_null]).to eq(false)
-      expect(email.first).to eq(:email)
-      expect(email.last[:db_type]).to eq('character varying(255)')
-      expect(email.last[:default]).to eq("'mail@example.com'::character varying")
-
-      id, title, user_id = DbSchema.connection.schema(:posts)
-      expect(id.first).to eq(:id)
-      expect(id.last[:db_type]).to eq('integer')
-      expect(id.last[:primary_key]).to eq(true)
-      expect(title.first).to eq(:title)
-      expect(title.last[:db_type]).to eq('character varying(255)')
-      expect(user_id.first).to eq(:user_id)
-      expect(user_id.last[:db_type]).to eq('integer')
-      expect(user_id.last[:allow_null]).to eq(false)
+        id, name, email = DbSchema.connection.schema(:users)
+        expect(id.first).to eq(:id)
+        expect(id.last[:db_type]).to eq('integer')
+        expect(id.last[:primary_key]).to eq(true)
+        expect(name.first).to eq(:name)
+        expect(name.last[:db_type]).to eq('character varying(255)')
+        expect(name.last[:allow_null]).to eq(false)
+        expect(email.first).to eq(:email)
+        expect(email.last[:db_type]).to eq('character varying(255)')
+        expect(email.last[:default]).to eq("'mail@example.com'::character varying")
+      end
     end
   end
 
