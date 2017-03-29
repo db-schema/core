14a624e6cebbcff4174c9ebc50d911d82fe5d4f6
Vsevolod Romashov <7@7vn.ru>
2017-03-29 11:03:47 +0300

    memoize Field::Custom.class_for

--- lib/db_schema/definitions/field/custom.rb
+++ lib/db_schema/definitions/field/custom.rb
@@ -6,7 +6,7 @@ module DbSchema
           def class_for(type_name)
             raise ArgumentError if type_name.nil?
 
-            Class.new(self) do
+            custom_types[type_name] ||= Class.new(self) do
               define_method :type do
                 type_name
               end
@@ -15,6 +15,11 @@ module DbSchema
                 type_name
               end
             end
+          end
+
+        private
+          def custom_types
+            @custom_types ||= {}
           end
         end
       end
