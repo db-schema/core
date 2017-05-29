6038d2bed0283543501c4518fe3742b89161d1e6
Vsevolod Romashov <7@7vn.ru>
2017-05-29 03:29:40 +0300

    add schema analysis methods to Definitions::Schema

--- lib/db_schema/definitions/schema.rb
+++ lib/db_schema/definitions/schema.rb
@@ -14,6 +14,18 @@ module DbSchema
       def [](table_name)
         tables.find { |table| table.name == table_name } || NullTable.new
       end
+
+      def has_table?(table_name)
+        !self[table_name].is_a?(NullTable)
+      end
+
+      def has_enum?(enum_name)
+        enums.any? { |enum| enum.name == enum_name }
+      end
+
+      def has_extension?(extension_name)
+        extensions.any? { |extension| extension.name == extension_name }
+      end
     end
   end
 end
